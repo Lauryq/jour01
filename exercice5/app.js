a96fdba8b08165622a44c7f5f5ce7079fa34150c
“laury <lquarto.lq@gmail.com>
2021-09-13 15:57:13 +0200

    avancement consigne 4 exe4

--- exercice5/app.js
+++ exercice5/app.js
@@ -1,6 +1,9 @@
 const section_1 = document.querySelector('.section1');
 const section_3 = document.querySelector('.section3')
 const carre = document.querySelector('.carre');
+const input = document.querySelector('input');
+const btn4 = document.querySelector('.btn_section4');
+const p4 = document.querySelector('.p_section4');
 const nom = 'Quarto';
 const prenom = 'Laury';
 
@@ -39,5 +42,45 @@ section_3.addEventListener('click', function(){
     p3.appendChild(txt3);
     p3.className = "resultat";
     section_3.appendChild(p3);
+
+})
+
+/*4ème section*/
+
+function maj(texte){
+
+    let temp = '';
+
+    for (let i = 0; i < texte.length; i++) {
+        
+        const element = texte[i];
+        let code = element.charCodeAt();
+        
+        /* to lower*/
+        if(code >= 65 && code <= 90){
+            let response = String.fromCharCode(code);
+            let min = response.toLowerCase();
+            temp = temp + min;
+        } 
+        /*to upper*/
+        else if (code >= 97 && code <= 122){
+            let response = String.fromCharCode(code);
+            let maj = response.toUpperCase();
+            temp = temp + maj;
+        }
+
+    }
+    
+    return temp;
+
+}
+
+btn4.addEventListener('click', function(){
+
+    p4.innerHTML = '';
+    let value = input.value;
+    let funct = maj(value);
+    let txt4 = document.createTextNode(`${funct}`);
+    p4.appendChild(txt4);
 
 })
